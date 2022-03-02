a46985ef1d9766f673bfb18f61bc68752b23b343
fcaramez <francisco.caramez.duarte@gmail.com>
2022-03-02 17:20:50 +0000

    lab done

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import { Divider, Input } from 'antd';
+import { useState } from 'react';
+
+// Iteration 5
+function SearchBar(props) {
+    const [search, setSearch] = useState('');
+
+    const handleChange = (e) => {
+      setSearch(e.target.value);
+      props.search(e.target.value);
+    };
+  return (
+    <div>
+      <Divider>Search</Divider>
+
+      <label>Search</label>
+      <Input value={search} type="text" onChange={handleChange} />
+    </div>
+  );
+}
+
+export default SearchBar;
